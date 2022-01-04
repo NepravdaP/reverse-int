6ae32581b7a61e0efbc7f7fd8d08a731ef424053
Pavel Harmaza <91322286+NepravdaP@users.noreply.github.com>
2022-01-04 18:57:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let absStr = Math.abs(n).toString();
+    let rev = Array.from(absStr, Number).reverse().join("");
+
+    return rev;
+};
+// let revNum = (n) => {
+//     let absStr = Math.abs(n).toString();
+//     let rev = parseInt(Array.from(absStr, Number).reverse().join(""), 10);
+
+//     return rev;
+// };
+// console.log(revNum(261));
